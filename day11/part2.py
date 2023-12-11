c4a41ca54ca957191f167b469c29f953ed76ca8a
jeff <jeffarends@gmail.com>
2023-12-11 15:53:23 -0700

    Altered manhattan distance to take into account crossing empty spaces

--- day11/part2.py
+++ day11/part2.py
@@ -12,33 +12,32 @@ sample_input = """...#......
 .......#..
 #...#....."""
 
-def expand_galaxy(lines: list):
+def find_empty_parts(lines: list):
     "Doubles any row or column in which there are no galaxies"
     # starting with columns
     width = len(lines[0])
     height = len(lines)
-    expandedColumns = []
-    expandedRows = []
-    expandedHorizontal = [[] for _ in range(height)]
+    emptyColumns = []
     for x in range(width):
         column = [line[x] for line in lines]
         doubleUp = "".join(column).find('#') == -1
-        for y in range(height):
-            expandedHorizontal[y].append(lines[y][x])
-            if doubleUp:
-                expandedHorizontal[y].append(lines[y][x])
-    outputLines = []
+        if doubleUp:
+            emptyColumns.append(x)
+    emptyRows = []
     for y in range(height):
-        joinedLine = "".join(expandedHorizontal[y])
-        outputLines.append(joinedLine)
-        if joinedLine.find('#') == -1:
-            outputLines.append(joinedLine)
-    return outputLines
+        if lines[y].find('#') == -1:
+            emptyRows.append(y)
+    return emptyRows, emptyColumns
     
-def manhattan_distance(location1, location2, expandedRows, expandedColumns, expansionMultiple):
+def manhattan_distance(location1, location2, emptyRows, emptyColumns, expansionMultiple):
     baseDistance = abs(location1[0] - location2[0]) + abs(location1[1] - location2[1])
-    horizontalRange = range(min(location1[0], location2[0]))
-    numHorizontalExpansions = len([])
+    minX = min(location1[0], location2[0])
+    maxX = max(location1[0], location2[0])
+    minY = min(location1[1], location2[1])
+    maxY = max(location1[1], location2[1])
+    numHorizontalExpansions = len([colNum for colNum in emptyColumns if minX < colNum < maxX])
+    numVerticalExpansions = len([rowNum for rowNum in emptyRows if minY < rowNum < maxY])
+    return baseDistance + (numHorizontalExpansions + numVerticalExpansions) * (expansionMultiple - 1)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -47,7 +46,7 @@ if __name__ == "__main__":
         with open(sys.argv[1]) as inputfile:
             lines = [line.strip() for line in inputfile.readlines()]
 
-    lines = expand_galaxy(lines)
+    emptyRows, emptyColumns = find_empty_parts(lines)
 
     # for line in lines:
     #     print(line)
@@ -66,10 +65,11 @@ if __name__ == "__main__":
     # add all the inter-galaxy connections
     totalDistance = 0
     connections = []
-    for galaxy1 in galaxies:
-        for galaxy2 in galaxies:
-            if galaxy1 != galaxy2:
-                heapq.heappush(connections, (manhattan_distance(galaxy1, galaxy2), galaxy1, galaxy2))
+    for i, galaxy1 in enumerate(galaxies):
+        for j, galaxy2 in enumerate(galaxies):
+            if i != j:
+                connection = (manhattan_distance(galaxy1, galaxy2, emptyRows, emptyColumns, 1000000), galaxy1, galaxy2)
+                heapq.heappush(connections, connection)
     connectedGalaxies = set()
     while len(connections) > 0:
         distance, galaxy1, galaxy2 = heapq.heappop(connections)
